caf8e3df2ea27c223ee871ea947f6da4e2529928
andrewuttley <andy.uttley@gmail.com>
2021-03-09 23:18:46 +0000

    added prediction percent to table

--- pga_app.py
+++ pga_app.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import numpy as np
+from PIL import Image
 
 #Create header
 st.write(
     """
-    # V1.2 of PGA App
-    ## Andy Uttley 
-    ### The following stats were scraped from the PGA website:
+    # PGA Data Modeller     
+    """)
+
+st.write(
     """
-)
-
-
+    ## Data scraped from PGA website    
+    """)
 #Bring in the data
 data = pd.read_excel('/Users/andrewuttley/Desktop/PGAapp/PGA_Database.xlsx')
 data
@@ -19,14 +21,10 @@ data
 #Create and name sidebar
 st.sidebar.header('Choose your weightings')
 
-thisyear = st.sidebar.slider('2021 weighting', 0.0, 1.0, 1.0, 0.2)
-lastyear = st.sidebar.slider('2020 weighting', 0.0, 1.0, 0.8, 0.2)
-st.sidebar.write("""Now choose your metric weightings:""")
-
 def user_input_features():
-    sgott = st.sidebar.slider('SG Off the Tee', 0.0, 2.0, 1.0, 0.5)
-    sgputt = st.sidebar.slider('SG Putting', 0.0, 2.0, 1.0, 0.5)
-    sgt2g = st.sidebar.slider('SG Tee to Green', 0.0, 2.0, 1.0, 0.5)
+    sgott = st.sidebar.slider('SG Off the Tee', 0, 100, 20, 5)
+    sgputt = st.sidebar.slider('SG Putting', 0, 100, 20, 5)
+    sgt2g = st.sidebar.slider('SG Tee to Green', 0, 100, 20, 5)
 
     user_data = {'SG OTT': sgott,
                  'SG T2G': sgt2g,
@@ -47,31 +45,48 @@ df_user
 #Output rankings based on users selections
 st.write(
     """
-    ## testing creating results dataframe
+    ## YOUR PREDICTION
     """
 )
 
+#image
+image = Image.open('/Users/andrewuttley/Desktop/PGAapp/Tiger.jpg')
+st.image(image)
+
 
 def results_output():
-    sg_ott = (data['SG_OTT_2020']*lastyear + data['SG_OTT_2021']*thisyear)/2 * df_user['SG OTT'][0]
-    sg_t2g = (data['SG_TeeToGreen_2020'] + data['SG_TeeToGreen_2021']) * df_user['SG T2G'][0]
-    sg_putt = (data['SG_Putting2020'] + data['SG_Putting2021']) * df_user['SG Putt'][0]
+    sg_ott = (data['SG_OTT_2020'] + data['SG_OTT_2021']) * df_user['SG OTT'][0]/100
+    sg_t2g = (data['SG_TeeToGreen_2020'] + data['SG_TeeToGreen_2021']) * df_user['SG T2G'][0]/100
+    sg_putt = (data['SG_Putting2020'] + data['SG_Putting2021']) * df_user['SG Putt'][0]/100
     results = {'Name': data['PLAYER NAME']
-               , 'Total': sg_ott + sg_t2g + sg_putt
+               , 'Total SG per round': sg_ott + sg_t2g + sg_putt
                , 'SG OTT Weighted': sg_ott
                ,'SG T2G Weighted': sg_t2g
                , 'SG Putt Weighted': sg_putt
                }
     resultpd = pd.DataFrame(results)
-    resultpd.sort_values(by=['Total'], ascending=False, inplace=True)
+    resultpd.sort_values(by=['Total SG per round'], ascending=False, inplace=True)
     return resultpd
 
 df_results  = results_output()
-df_results
+
+
+# use softmax to create the % probability
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return (e_x / e_x.sum(axis=0))*100
+
+df_results['Win prediction %'] = softmax(df_results['Total SG per round'])
+df_results2 = df_results[['Name', 'Win prediction %', 'Total SG per round']]
+df_results2
+
+
+
+
 
 #try making a graph
 chart = alt.Chart(df_results).mark_bar().encode(
-    alt.X("Total", bin=False),
+    alt.X("Total SG per round", bin=False),
     y='Name',
 )
 st.altair_chart(chart)
