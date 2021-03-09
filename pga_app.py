8a2a7a42369df04c9ac92851abe2452970c96de3
andrewuttley <andy.uttley@gmail.com>
2021-03-09 23:46:15 +0000

    added more parameters

--- pga_app.py
+++ pga_app.py
@@ -23,11 +23,15 @@ st.sidebar.header('Choose your weightings')
 
 def user_input_features():
     sgott = st.sidebar.slider('SG Off the Tee', 0, 100, 20, 5)
-    sgputt = st.sidebar.slider('SG Putting', 0, 100, 20, 5)
     sgt2g = st.sidebar.slider('SG Tee to Green', 0, 100, 20, 5)
+    sga2g = st.sidebar.slider('SG Approach to Green', 0, 100, 20, 5)
+    sgatg = st.sidebar.slider('SG Around the Green', 0, 100, 20, 5)
+    sgputt = st.sidebar.slider('SG Putting', 0, 100, 20, 5)
 
     user_data = {'SG OTT': sgott,
                  'SG T2G': sgt2g,
+                 'SG A2G': sga2g,
+                 'SG ATG': sgatg,
                  'SG Putt': sgputt}
     features = pd.DataFrame(user_data, index=[0])
     return features
@@ -57,11 +61,15 @@ st.image(image)
 def results_output():
     sg_ott = (data['SG_OTT_2020'] + data['SG_OTT_2021']) * df_user['SG OTT'][0]/100
     sg_t2g = (data['SG_TeeToGreen_2020'] + data['SG_TeeToGreen_2021']) * df_user['SG T2G'][0]/100
+    sg_a2g = (data['SG_A2G_2020'] + data['SG_A2G_2021']) * df_user['SG A2G'][0] / 100
+    sg_atg = (data['SG_ATG_2020'] + data['SG_ATG_2021']) * df_user['SG ATG'][0] / 100
     sg_putt = (data['SG_Putting2020'] + data['SG_Putting2021']) * df_user['SG Putt'][0]/100
     results = {'Name': data['PLAYER NAME']
-               , 'Total SG per round': sg_ott + sg_t2g + sg_putt
+               , 'Total SG per round': sg_ott + sg_t2g + sg_a2g + sg_atg + sg_putt
                , 'SG OTT Weighted': sg_ott
-               ,'SG T2G Weighted': sg_t2g
+               , 'SG A2G Weighted': sg_a2g
+               , 'SG T2G Weighted': sg_t2g
+               , 'SG ATG Weighted': sg_atg
                , 'SG Putt Weighted': sg_putt
                }
     resultpd = pd.DataFrame(results)
