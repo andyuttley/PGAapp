4da8c9f7bc4fcc0f6fe85211d9c292817abb39c7
andrewuttley <andy.uttley@gmail.com>
2021-03-12 17:30:35 +0000

    trying to add new update
    Signed-off-by: andrewuttley <andy.uttley@gmail.com>

--- pga_app.py
+++ pga_app.py
@@ -110,5 +110,4 @@ df_results['Win prediction %'] = softmax(df_results['Total SG per round'])
 df_results2 = df_results[['Name', 'Win prediction %', 'Total SG per round']]
 df_results2
 
-#gitpush
 
