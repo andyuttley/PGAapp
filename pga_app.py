3c1ccfb6af6aee486430f700819aa7d85430e43a
andrewuttley <andy.uttley@gmail.com>
2021-03-21 09:38:36 +0000

    added masters history
    Signed-off-by: andrewuttley <andy.uttley@gmail.com>

--- pga_app.py
+++ pga_app.py
@@ -9,13 +9,11 @@ linkedinlink = '[Andy Uttley - LinkedIn](https://www.linkedin.com/in/andrewuttle
 mediumlink = '[Andy Uttley - Medium Blog](https://andy-uttley.medium.com/)'
 
 #Create header
-st.write("""# PGA Data Modeller
-    ## How it works
-    Using live data scraped from the PGA website, model your predicted winner by using the left side of the screen to apply  weightings to the different metrics. This will give you a ranked 'predicted outcome' based on your selections.
-    
-    The current selections are those deemed most appropriate to the Masters based on recent outcomes. 
-    
-    ## For more information visit:
+st.write("""# PGA Data Modeller""")
+st.write("""## How it works""")
+st.write("Using live data scraped from the PGA website, model your predicted winner by using the left side of the screen to apply  weightings to the different metrics. This will give you a ranked 'predicted outcome' based on your selections. "
+         "The current selections are those deemed most appropriate to the Masters based on recent outcomes.")
+st.write("""## For more information visit:
     """, mediumlink, """ | """,
     linkedinlink)
 
