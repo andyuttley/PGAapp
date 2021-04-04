2f6f90c938bf781edc4eb827beec42accbadba6f
andrewuttley <andy.uttley@gmail.com>
2021-04-04 23:15:22 +0100

    AESTHETIC CHANGES
    Signed-off-by: andrewuttley <andy.uttley@gmail.com>

--- pga_app.py
+++ pga_app.py
@@ -34,12 +34,12 @@ def user_input_features():
     sgott = st.sidebar.slider('SG Off the Tee', 0, 100, 70, 5)
     sga2g = st.sidebar.slider('SG Approach to Green', 0, 100, 90, 5)
     sgatg = st.sidebar.slider('SG Around the Green', 0, 100, 50, 5)
-    sgputt = st.sidebar.slider('SG Putting', 0, 100, 80, 5)
+    sgputt = st.sidebar.slider('SG Putting', 0, 100, 25, 5)
     sgmasters = st.sidebar.slider('SG Masters History', 0, 100, 80, 5)
     sgtotal = st.sidebar.slider('SG Total', 0, 100, 25, 5)
     sgpar5 = st.sidebar.slider('SG Par 5s', 0, 100, 75, 5)
-    sgpar4 = st.sidebar.slider('SG Par 4s', 0, 100, 15, 5)
-    sgpar3 = st.sidebar.slider('SG Par 3s', 0, 100, 15, 5)
+    sgpar4 = st.sidebar.slider('SG Par 4s', 0, 100, 25, 5)
+    sgpar3 = st.sidebar.slider('SG Par 3s', 0, 100, 20, 5)
 
 
 
